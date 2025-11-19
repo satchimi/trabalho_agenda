8c37ad09b8d462e4c6d8fbe3c2b4b7f34f82fbf6
Satchi <thiagoabreu429@gmail.com>
2025-11-19 17:45:44 -0300

    updates e funcao deletar contato

--- src/agenda.c
+++ src/agenda.c
@@ -2,69 +2,96 @@
 #include <stdlib.h>
 #include <string.h>
 #include "agenda.h"
+#include "leitor_string.h"
 
-int adicionarContato(Contato *c, int quant)
+int adicionarContato(Contato contatos[], int quant)
 {
+    int i = 0;
+
     if (quant < MAX_VETOR)
     {
         printf("\n--- Cadastro de Contato ---\n");
 
         printf("\nDigite o nome do contato a ser salvo: ");
-        scanf("%100[^\n]", c[quant].nome);
+        ler_linha(contatos[quant].nome, MAX_NOME);
 
-        printf("\nDigite o seu telefone: ");
-        scanf("%s", c[quant].telefone);
-
+        printf("Digite o seu telefone: ");
+        ler_linha(contatos[quant].telefone, MAX_TELEFONE);
+        
         printf("Digite o seu e-mail: ");
-        scanf("%s", c[quant].email);
+        ler_palavra(contatos[quant].email, MAX_EMAIL);
 
-        return 1;
+        printf("\n");
+
+        i = 1;
     }
     else
-    {
         printf("\nA agenda está cheia.\n");
-        return 0;
-    }
+    
+    system("cls");
+    return i;
 }
 
-void QntContatos(Contato *c, int quant)
+void listaContatos(Contato contatos[], int quant)
 {
     printf("\n--- Lista de Contatos ---\n");
 
-    if (quant == 0)
-    {
-        printf("Nenhum contato cadastrado.\n");
+    if (quant == 0) {
+        printf("\nNenhum contato cadastrado.\n");
+
         return;
     }
 
-    for (int i = 0; i < quant; i++)
-    {
-        printf("\nContato %d:\n", i + 1);
-        printf("Nome: %s\n", c[i].nome);
-        printf("Telefone: %s\n", c[i].telefone);
-        printf("E-mail: %s\n", c[i].email);
-        printf("\n");
+    for (int i = 0; i < quant; i++) {
+        printf("\n[Contato %d]\n", i + 1);
+        printf("Nome: %s\n", contatos[i].nome);
+        printf("Telefone: %s\n", contatos[i].telefone);
+        printf("E-mail: %s\n\n", contatos[i].email);
     }
 }
 
-void pesquisarContato(Contato *c, int quant)
+void pesquisarContato(Contato contatos[], int quant)
 {
     char nomeBuscado[MAX_NOME];
 
     printf("\nDigite o nome que deseja pesquisar: ");
-    scanf("%s", nomeBuscado);
+    ler_linha(nomeBuscado, MAX_NOME);
 
-    for (int i = 0; i < quant; i++)
-    {
-        if (strcmp(nomeBuscado, c[i].nome) == 0)
-        {
+    for (int i = 0; i < quant; i++) {
+        if (strcmp(nomeBuscado, contatos[i].nome) == 0) {
             printf("\n--- Contato Encontrado ---\n");
-            printf("Nome: %s\n", c[i].nome);
-            printf("Telefone: %s\n", c[i].telefone);
-            printf("E-mail: %s\n", c[i].email);
+            printf("Nome: %s\n", contatos[i].nome);
+            printf("Telefone: %s\n", contatos[i].telefone);
+            printf("E-mail: %s\n\n", contatos[i].email);
+
             return;
         }
     }
 
-    printf("\nContato não encontrado.\n");
+    printf("\nContato nao encontrado.\n\n");
+}
+
+void deletarContato(Contato contatos[], int* quant)
+{
+    int id;
+
+    listaContatos(contatos, *quant);
+
+    printf("Digite o numero do contato que voce quer deletar: ");
+    scanf(" %d", &id);
+
+    while(getchar() != '\n');
+
+    if (id < 1 || id > *quant) {
+        printf("\nNumero invalido!\n\n");
+        return;
+    }
+
+    for ( ; id < *quant; id++) {
+        contatos[id - 1] = contatos[id];
+    }
+
+    (*quant)--;
+
+    printf("\n");
 }
